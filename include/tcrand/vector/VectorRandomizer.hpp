fa9c0d7fd8604e906c6e20898b4af7bcd6fcf032
Alham Fikri Aji <afaji321@gmail.com>
2017-06-06 21:49:14 +0100

    refactor vector randomizer

--- include/tcrand/vector/VectorRandomizer.hpp
+++ include/tcrand/vector/VectorRandomizer.hpp
@@ -16,29 +16,89 @@ namespace tcrand {
 
     template <class T>
     class VectorRandomizer{
-        vector<T> result;
-        function<T(void)> generator;
-        map<T, int> occurences;
         int param_length;
+        int param_distinct_elements;
+        int param_min_count;
+        int param_max_count;
+        bool param_is_unique;
 
-        int _distinct_elements;
-        bool is_unique;
+        function<T(void)> generator;
 
 
-        void load_params(){
-            occurences.clear();
-            if (_distinct_elements == -1 && is_unique){
-                _distinct_elements = param_length;
-            }
+        T next_random_element(){
+            return generator();
         }
 
+        vector<T> __next(){
+            int fail_attempt = 0;
+            vector<T> result;
+            map<T, int> counts;
+            vector<T> result_set;
+
+            while (fail_attempt < 1000 && result.size() < param_length){
+                //attempt to add a possibly new element:
+                if (result_set.size() < param_distinct_elements || param_distinct_elements == -1){
+                    T element = next_random_element();
+                    cout<<"AA "<<element<<endl;
+
+                    //if exists before, try again
+                    if ((param_is_unique && counts[element]) || (counts[element] + 1 > param_max_count)){
+                        fail_attempt++;
+                        continue;
+                    }
+
+                    //if adding it makes the array longer, try again
+                    //if exists before, try again
+                    if (result.size() + param_min_count > param_length){
+                        fail_attempt++;
+                        continue;
+                    }
+
+                    //if this element is not in our current result, add this by "param_min_count"
+                    //otherwise, just add one
+                    int add_amount = 1;
+                    if (counts[element] == 0) {
+                        result_set.push_back(element);
+                        add_amount = param_min_count;
+                    }
+
+                    for (int i = 0;i < add_amount; i++) {
+                        result.push_back(element);
+                        counts[element]++;
+                    }
+                }
+
+                //attempt to add an old element:
+                else if (result_set.size() > 0 && param_is_unique == false){
+                    int pick_id = rand_int(result_set.size());
+                    T element = result_set[pick_id];
+
+                    counts[element]++;
+                    result.push_back(element);
+
+                    if (counts[element] == param_max_count) {
+                        swap(result_set[pick_id], result_set[result_set.size() - 1] );
+                        result_set.pop_back();
+                    }
+                } else {
+                    fail_attempt++;
+                }
+            }
+            
+            if (fail_attempt == 1000)
+                throw runtime_error("failed to generate vector");
+
+            random_shuffle(result.begin(), result.end());
+            return result;
+        }
 
     public:
-
         VectorRandomizer(){
             param_length = 10;
-            is_unique = false;
-            _distinct_elements = -1;
+            param_is_unique = false;
+            param_distinct_elements = -1;
+            param_max_count = 9999999;
+            param_min_count = 1;
         }
 
         VectorRandomizer& length(int n){
@@ -46,56 +106,34 @@ namespace tcrand {
             return *this;
         }
 
+        VectorRandomizer& unique(bool u){
+            param_is_unique = u;
+            param_distinct_elements = -1;
+            return *this;
+        }
+
+        VectorRandomizer& distinct_elements(int n){
+            param_is_unique = false;
+            param_distinct_elements = n;
+            return *this;
+        }
+
+        VectorRandomizer& element_counts(int min_count, int max_count){
+            param_min_count = min_count;
+            param_max_count = max_count;
+        }
+
+        VectorRandomizer& engine(){
+
+        }
+
         VectorRandomizer& engine(function<T(void)> f){
             generator = f;
             return *this;
         }
 
-        VectorRandomizer& unique(bool u){
-            is_unique = u;
-            _distinct_elements = -1;
-            return *this;
-        }
-
-        VectorRandomizer& distinct_elements(int n){
-            is_unique = true;
-            _distinct_elements = n;
-            return *this;
-        }
-
-
         vector<T> next(){
-            load_params();
-            if (!generator){
-                throw runtime_error("please define your engine");
-            }
-            result.clear();
-
-            //create all valid sets
-            int options = 0;
-            if (is_unique && _distinct_elements > 0){
-                while (result.size() < _distinct_elements){
-                    T res = (T) generator();
-                    if (occurences[res])
-                        continue;
-                    occurences[res]++;
-                    result.push_back(res);
-                    options++;
-                }
-            }
-            
-            while (result.size() < param_length ){
-                T res;
-                if (_distinct_elements > 0){
-                    res = result[ rand_int(options) ];
-                } else {
-                    res = (T) generator();
-                }
-
-                result.push_back(res);
-            }
-            random_shuffle(result.begin(), result.end());
-            return result;
+            return __next();
         }
     };
 }
